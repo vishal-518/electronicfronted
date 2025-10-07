76cc413e4b5e185a1d0201662654a384a5c90582
MAC <mac@MACs-MBP.bbrouter>
2025-10-07 11:53:19 +0530

    Add Store.js

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/Store.js'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
